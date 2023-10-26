80e8b8bae983e1802d23d773f7ae13800465459b
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2023-10-26 15:29:08 -0300

    fiz o html da página principal.

--- apps/manter_alunos/admin.py
+++ apps/manter_alunos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Aluno, Cidade, Curso
 
 # Register your models here.
+admin.site.register(Aluno)
+admin.site.register(Cidade)
+admin.site.register(Curso)
--- apps/manter_alunos/models.py
+++ apps/manter_alunos/models.py
@@ -5,9 +5,15 @@ class Cidade(models.Model):
     nome = models.CharField(max_length=30)
     sigla_estado = models.CharField(max_length=2)
 
+    def __str__(self):
+        return self.nome
+
 
 class Curso(models.Model):
     nome = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.nome
 
 class Aluno(models.Model):
     nome = models.CharField(max_length=40)
@@ -16,5 +22,8 @@ class Aluno(models.Model):
     data_de_nascimento = models.CharField(max_length=60)
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
     cidade = models.ForeignKey(Cidade, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
 
 
