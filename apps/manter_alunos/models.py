53841eb61edf3c89fbeb92e2a0292d5e76037efa
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2023-10-26 15:13:42 -0300

    commit inicial.

---
+++ apps/manter_alunos/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Cidade(models.Model):
+    nome = models.CharField(max_length=30)
+    sigla_estado = models.CharField(max_length=2)
+
+
+class Curso(models.Model):
+    nome = models.CharField(max_length=15)
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=40)
+    endereco = models.CharField(max_length=70)
+    email = models.CharField(max_length=40)
+    data_de_nascimento = models.CharField(max_length=60)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    cidade = models.ForeignKey(Cidade, on_delete=models.CASCADE)
+
+
